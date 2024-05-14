5a0f599164e6eb3647cb65176dc2d1882909919a
ChElIk69 <s953369@gmail.com>
2024-05-15 00:54:40 +0300

    init: start project

---
+++ Test/Задание 2/Задание 2.cpp
@@ -0,0 +1,51 @@
+﻿/*Даны действительные числа a1, ... , an. Требуется умножить все члены
+последовательности a1, ... , an на квадрат ее наименьшего числа, если
+ai ≥ 0, и на квадрат ее наибольшего числа, если ai < 0.*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(0, "ru");
+    int n;
+    cout << "Введите количество элементов: ";
+    cin >> n;
+    double* arr = new double[n];
+
+
+for (int i = 0; i < n; i++) {
+    arr[i] = rand() % 100 - 50;
+    cout << arr[i] << ' ' << endl;
+}
+
+double minNum = arr[0];
+double maxNum = arr[0];
+
+
+for (int i = 1; i < n; i++) {
+    if (arr[i] < minNum) {
+        minNum = arr[i];
+    }
+    if (arr[i] > maxNum) {
+        maxNum = arr[i];
+    }
+}
+
+
+for (int i = 0; i < n; i++) {
+    if (arr[i] >= 0) {
+        arr[i] *= minNum * minNum;
+    } else {
+        arr[i] *= maxNum * maxNum;
+    }
+}
+
+cout << "Модифицированный массив: ";
+for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+}
+
+delete[] arr;
+
+return 0;
+}
